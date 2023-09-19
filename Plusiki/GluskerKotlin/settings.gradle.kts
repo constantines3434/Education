90b830b7c9bcffea6abc926e57a43fdcc63d7e90
Constantine Smirnov <constantines748@gmail.com>
2023-09-19 16:59:14 +0300

    ex1

---
+++ Plusiki/GluskerKotlin/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GluskerPractice1"
+include(":app")
